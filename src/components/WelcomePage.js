21be681a5bfc46fe3944b99cf335c49e556e8f8f
loadalready <justaviram@gmail.com>
2018-08-28 10:13:53 -0400

    remove read me, will update this later

---
+++ src/components/WelcomePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class WelcomePage extends Component {
+  render() {
+    return (
+      <div className="welcome-component">
+        <h1 className="welcome-component-title">WELCOME</h1>
+      </div>
+    )
+  }
+}
---
+++ src/containers/Welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import WelcomePage from '../components/WelcomePage'
+
+export default class Welcome extends Component {
+  render() {
+    return (
+      <div className="welcome-container">
+        <WelcomePage />
+      </div>
+    )
+  }
+}
