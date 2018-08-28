ac083d989391397b2406fd38992456950734e1b9
Avi Cohen <justaviram@gmail.com>
2018-08-28 11:54:15 -0400

    added animate css library and tested it in welcomepage component

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
 export default class WelcomePage extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      currentEffect: 'animated jello infinite delay-1s welcome-component-title'
+    }
+  }
   render() {
     return (
       <div className="welcome-component">
-        <h1 className="welcome-component-title">WELCOME</h1>
+        <h1 className= {this.state.currentEffect}>WELCOME</h1>
       </div>
     )
   }
